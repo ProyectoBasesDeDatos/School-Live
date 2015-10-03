665e46c17c450f9509e40afebc091e2bf8ac07ce
Fabian Ugalde <fugar09@gmail.com>
2015-10-03 14:57:37 -0600

    Prueba agregue asignaciones

--- src/Interface/Escritorio.java
+++ src/Interface/Escritorio.java
@@ -38,6 +38,7 @@ public class Escritorio extends javax.swing.JFrame {
         jMenu3 = new javax.swing.JMenu();
         jMenu5 = new javax.swing.JMenu();
         jMenu7 = new javax.swing.JMenu();
+        jMenuItem3 = new javax.swing.JMenuItem();
         jMenu8 = new javax.swing.JMenu();
         jMenu2 = new javax.swing.JMenu();
         jMenuItem1 = new javax.swing.JMenuItem();
@@ -77,6 +78,15 @@ public class Escritorio extends javax.swing.JFrame {
 
         jMenu7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Iconos/clipboard.png")));
         jMenu7.setText("Asignaciones");
+
+        jMenuItem3.setText("Asignacion");
+        jMenuItem3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem3ActionPerformed(evt);
+            }
+        });
+        jMenu7.add(jMenuItem3);
+
         jMenuBar1.add(jMenu7);
 
         jMenu8.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Iconos/3d bar chart.png")));
@@ -143,6 +153,13 @@ public class Escritorio extends javax.swing.JFrame {
         new Busqueda().setVisible(true);
     }//GEN-LAST:event_jMenuItem2ActionPerformed
 
+    private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
+        Asignaciones asignaciones = new Asignaciones();
+        jDesktopPane1.add(asignaciones);
+        asignaciones.show();
+        
+    }//GEN-LAST:event_jMenuItem3ActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -192,6 +209,7 @@ public class Escritorio extends javax.swing.JFrame {
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem1;
     private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JMenuItem jMenuItem3;
     // End of variables declaration//GEN-END:variables
 
 }
