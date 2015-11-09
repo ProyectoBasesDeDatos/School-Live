c7ac0ebc4f40fc19cc1a77d4be93745f50352310
rpgki <solfonca@gmail.com>
2015-11-08 21:46:31 -0600

    Creación Métodos Ver/Editar Perfil Est/Padre
    Se agregan los métodos para ver y actualizar los campos del perfil del
    estudiante y del padre de familia. Aún hay que revisar unas cosas que no
    supe como hacer, sin embargo se puede hacer la consulta al profe y/o al
    asistente.

--- src/BaseDatos/ConexionBase.java
+++ src/BaseDatos/ConexionBase.java
@@ -79,4 +79,209 @@ public class ConexionBase {
         }
         
     }
+    
+    public String obtieneIdentificacion(String idPersona, String Parametro) {
+    
+        String sqlIdentificacion = "select InformacionPerfilEstudiantePadreFamilia(?,?);";
+        PreparedStatement sentencia = null;
+        try {
+            sentencia = base.prepareStatement(sqlIdentificacion);
+            sentencia.setString(1, idPersona);
+            sentencia.setString(2, Parametro);
+            sentencia.execute();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    return sqlIdentificacion;    
+    }
+    
+    public String obtieneNombre(String idPersona, String Parametro) {
+    
+        String sqlNombre = "select InformacionPerfilEstudiantePadreFamilia(?,?);";
+        PreparedStatement sentencia = null;
+        try {
+            sentencia = base.prepareStatement(sqlNombre);
+            sentencia.setString(1, idPersona);
+            sentencia.setString(2, Parametro);
+            sentencia.execute();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    return sqlNombre;    
+    }
+    
+    public String obtieneApellido1(String idPersona, String Parametro) {
+    
+        String sqlApellido1 = "select InformacionPerfilEstudiantePadreFamilia(?,?);";
+        PreparedStatement sentencia = null;
+        try {
+            sentencia = base.prepareStatement(sqlApellido1);
+            sentencia.setString(1, idPersona);
+            sentencia.setString(2, Parametro);
+            sentencia.execute();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    return sqlApellido1;    
+    }
+    
+    public String obtieneApellido2(String idPersona, String Parametro) {
+    
+        String sqlApellido2 = "select InformacionPerfilEstudiantePadreFamilia(?,?);";
+        PreparedStatement sentencia = null;
+        try {
+            sentencia = base.prepareStatement(sqlApellido2);
+            sentencia.setString(1, idPersona);
+            sentencia.setString(2, Parametro);
+            sentencia.execute();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    return sqlApellido2;    
+    }
+    
+    public String obtieneSexo(String idPersona, String Parametro) {
+    
+        String sqlSexo = "select InformacionPerfilEstudiantePadreFamilia(?,?);";
+        PreparedStatement sentencia = null;
+        try {
+            sentencia = base.prepareStatement(sqlSexo);
+            sentencia.setString(1, idPersona);
+            sentencia.setString(2, Parametro);
+            sentencia.execute();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    return sqlSexo;    
+    }
+    
+    public String obtieneFechaNacimiento(String idPersona, String Parametro) {
+    
+        String sqlFechaNacimiento = "select InformacionPerfilEstudiantePadreFamilia(?,?);";
+        PreparedStatement sentencia = null;
+        try {
+            sentencia = base.prepareStatement(sqlFechaNacimiento);
+            sentencia.setString(1, idPersona);
+            sentencia.setString(2, Parametro);
+            sentencia.execute();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    return sqlFechaNacimiento;    
+    }
+    
+    public String obtieneEmail(String idPersona, String Parametro) {
+    
+        String sqlEmail = "select InformacionPerfilEstudiantePadreFamilia(?,?);";
+        PreparedStatement sentencia = null;
+        try {
+            sentencia = base.prepareStatement(sqlEmail);
+            sentencia.setString(1, idPersona);
+            sentencia.setString(2, Parametro);
+            sentencia.execute();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    return sqlEmail;    
+    }
+    
+    public String obtieneFacebook(String idPersona, String Parametro) {
+    
+        String sqlFacebook = "select InformacionPerfilEstudiantePadreFamilia(?,?);";
+        PreparedStatement sentencia = null;
+        try {
+            sentencia = base.prepareStatement(sqlFacebook);
+            sentencia.setString(1, idPersona);
+            sentencia.setString(2, Parametro);
+            sentencia.execute();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    return sqlFacebook;    
+    }
+    
+    public String obtieneTipoPerfil(String idPersona, String Parametro) {
+    
+        String sqlTipoPerfil = "select InformacionPerfilEstudiantePadreFamilia(?,?);";
+        PreparedStatement sentencia = null;
+        try {
+            sentencia = base.prepareStatement(sqlTipoPerfil);
+            sentencia.setString(1, idPersona);
+            sentencia.setString(2, Parametro);
+            sentencia.execute();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    return sqlTipoPerfil;    
+    }
+    
+    public String obtieneDireccion(String idPersona, String Parametro) {
+    
+        String sqlDireccion = "select InformacionPerfilEstudiantePadreFamilia(?,?);";
+        PreparedStatement sentencia = null;
+        try {
+            sentencia = base.prepareStatement(sqlDireccion);
+            sentencia.setString(1, idPersona);
+            sentencia.setString(2, Parametro);
+            sentencia.execute();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    return sqlDireccion;
+    }
+    
+    public String actualizaDatosEstudiantePadreFamilia(String IdPersona, String Email, String Facebook) {
+    
+        String sqlactualizaest = "update Persona set email = ?, facebook = ? where idpersona = ?;";
+        PreparedStatement sentencia = null;
+        try {
+            sentencia = base.prepareStatement(sqlactualizaest);
+            sentencia.setString(1, Email);
+            sentencia.setString(2, Facebook);
+            sentencia.setString(3, IdPersona);
+            sentencia.execute();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    return sqlactualizaest;
+    }
+    
+    public String actualizaDireccionEstudiantePadreFamilia(String IdPersona, String Direccion) {
+    
+        String sqlactualizaestdir = "update DirPersona set descripcion = ? where idpersona = ?;";
+        PreparedStatement sentencia = null;
+        try {
+            sentencia = base.prepareStatement(sqlactualizaestdir);
+            sentencia.setString(1, Direccion);
+            sentencia.setString(2, IdPersona);
+            sentencia.execute();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    return sqlactualizaestdir;
+    }
 }
